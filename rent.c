f36de5b31b847eb4f212ad65aa909413e4dd041c
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-07-31 13:35:02 +0200

    Compute value for two incompatible orders

--- rent.c
+++ rent.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_ORDERS 10000
+
 void process_cases(int nb_cases);
-void process_orders(int nb_orders);
+void compute_value(int nb_orders);
+void scan_orders(int nb_orders);
 
 int main() {
     int nb_cases;
@@ -16,25 +19,41 @@ void process_cases(int nb_cases) {
     for (int test_case = 0; test_case < nb_cases; ++test_case) {
         int nb_orders;
         scanf("%d", &nb_orders);
-        process_orders(nb_orders);
+        compute_value(nb_orders);
     }
 }
 
-void process_orders(int nb_orders) {
+static struct {
+    int start_time;
+    int duration;
+    int price;
+} Orders[MAX_ORDERS];
+
+void compute_value(int nb_orders) {
+    scan_orders(nb_orders);
+
     int value = 0;
-    for (int i = 0; i < nb_orders; ++i) {
-        struct {
-            int start_time;
-            int duration;
-            int price;
-        } order;
-
-        scanf("%d %d %d",
-                &order.start_time,
-                &order.duration,
-                &order.price);
-
-        value += order.price;
+    if (nb_orders == 1) {
+        value = Orders[0].price;
+    }
+    else {
+        if (Orders[1].start_time >= Orders[0].start_time + Orders[0].duration) {
+            value = Orders[0].price +  Orders[1].price;
+        }
+        else {
+            value = Orders[0].price > Orders[1].price
+                ? Orders[0].price
+                : Orders[1].price;
+        }
     }
     printf("%d\n", value);
+}
+
+void scan_orders(int nb_orders) {
+    for (int i = 0; i < nb_orders; ++i) {
+        scanf("%d %d %d",
+                &Orders[i].start_time,
+                &Orders[i].duration,
+                &Orders[i].price);
+    }
 }
