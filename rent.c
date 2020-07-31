00f43220827f28fa3d95d56d832ccd9f6d96ebdd
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-07-31 13:18:58 +0200

    Compute value for two compatible orders

--- rent.c
+++ rent.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void process_cases(int nb_cases);
+void process_orders(int nb_orders);
+
 int main() {
-    int value = 100;
-    printf("%d\n", value);
+    int nb_cases;
+    scanf("%d", &nb_cases);
+    process_cases(nb_cases);
+
     return EXIT_SUCCESS;
+}
+
+void process_cases(int nb_cases) {
+    for (int test_case = 0; test_case < nb_cases; ++test_case) {
+        int nb_orders;
+        scanf("%d", &nb_orders);
+        process_orders(nb_orders);
+    }
+}
+
+void process_orders(int nb_orders) {
+    int value = 0;
+    for (int i = 0; i < nb_orders; ++i) {
+        struct {
+            int start_time;
+            int duration;
+            int price;
+        } order;
+
+        scanf("%d %d %d",
+                &order.start_time,
+                &order.duration,
+                &order.price);
+
+        value += order.price;
+    }
+    printf("%d\n", value);
 }
