0c3ffb36afc393b5f293a27f8ab219b7d98eff22
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-08-01 09:03:30 +0200

    Remove static variable

--- rent.c
+++ rent.c
@@ -9,53 +9,56 @@ struct order {
     int price;
 };
 
-static struct order Orders[MAX_ORDERS];
-
 void process_cases(int nb_cases);
-void compute_value(int nb_orders);
-void scan_orders(int nb_orders);
+void compute_value(int nb_orders, struct order orders[]);
+void scan_orders(int nb_orders, struct order orders[]);
 
 int main() {
     int nb_cases;
     scanf("%d", &nb_cases);
+
     process_cases(nb_cases);
 
     return EXIT_SUCCESS;
 }
 
 void process_cases(int nb_cases) {
+    struct order orders[MAX_ORDERS];
+
     for (int test_case = 0; test_case < nb_cases; ++test_case) {
         int nb_orders;
         scanf("%d", &nb_orders);
-        compute_value(nb_orders);
+        compute_value(nb_orders, orders);
     }
 }
 
-void compute_value(int nb_orders) {
-    scan_orders(nb_orders);
+void compute_value(int nb_orders, struct order orders[]) {
+
+    scan_orders(nb_orders, orders);
 
     int value = 0;
     if (nb_orders == 1) {
-        value = Orders[0].price;
+        value = orders[0].price;
     }
     else {
-        if (Orders[1].start_time >= Orders[0].start_time + Orders[0].duration) {
-            value = Orders[0].price +  Orders[1].price;
+        if (orders[1].start_time >= orders[0].start_time + orders[0].duration) {
+            value = orders[0].price +  orders[1].price;
         }
         else {
-            value = Orders[0].price > Orders[1].price
-                ? Orders[0].price
-                : Orders[1].price;
+            value = orders[0].price > orders[1].price
+                ? orders[0].price
+                : orders[1].price;
         }
     }
+
     printf("%d\n", value);
 }
 
-void scan_orders(int nb_orders) {
+void scan_orders(int nb_orders, struct order orders[]) {
     for (int i = 0; i < nb_orders; ++i) {
         scanf("%d %d %d",
-                &Orders[i].start_time,
-                &Orders[i].duration,
-                &Orders[i].price);
+                &orders[i].start_time,
+                &orders[i].duration,
+                &orders[i].price);
     }
 }
