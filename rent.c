5e7669742fa0f6ffd45b3b06df46921ef80dd8d0
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-08-03 14:21:15 +0200

    Use typedef to define Order struct

--- rent.c
+++ rent.c
@@ -5,21 +5,21 @@
 #define MAX_ORDERS 10000
 #define SENTINEL_START_TIME 2000000
 
-struct order {
+typedef struct {
     int start_time;
     int duration;
     int price;
-};
+} Order;
 
 static inline int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
 void process_cases(int nb_cases);
-void scan_orders(int nb_orders, struct order orders[]);
-int compute_value(int nb_orders, struct order orders[]);
-struct order *next_compatible_order(int position, struct order *current, struct order orders[]);
-bool does_end_before_start(struct order *first, struct order *second);
+void scan_orders(int nb_orders, Order orders[]);
+int compute_value(int nb_orders, Order orders[]);
+Order *next_compatible_order(int position, Order *current, Order orders[]);
+bool does_end_before_start(Order *first, Order *second);
 
 int main() {
     int nb_cases;
@@ -31,7 +31,7 @@ int main() {
 }
 
 void process_cases(int nb_cases) {
-    struct order orders[MAX_ORDERS+1];
+    Order orders[MAX_ORDERS+1];
 
     for (int test_case = 0; test_case < nb_cases; ++test_case) {
         int nb_orders;
@@ -44,7 +44,7 @@ void process_cases(int nb_cases) {
     }
 }
 
-void scan_orders(int nb_orders, struct order orders[]) {
+void scan_orders(int nb_orders, Order orders[]) {
     for (int i = 0; i < nb_orders; ++i) {
         scanf("%d %d %d",
                 &orders[i].start_time,
@@ -57,11 +57,11 @@ void scan_orders(int nb_orders, struct order orders[]) {
     orders[nb_orders].price = 0;
 }
 
-int compute_value(int nb_orders, struct order orders[]) {
+int compute_value(int nb_orders, Order orders[]) {
     for(int i = nb_orders -1 ; i >= 0; --i) {
-        struct order *current = &orders[i];
-        struct order *next = &orders[i+1];
-        struct order *next_compatible = next_compatible_order(i+1, current, orders);
+        Order *current = &orders[i];
+        Order *next = &orders[i+1];
+        Order *next_compatible = next_compatible_order(i+1, current, orders);
 
         current->price = max(current->price + next_compatible->price, next->price);
     }
@@ -69,9 +69,9 @@ int compute_value(int nb_orders, struct order orders[]) {
     return orders[0].price;
 }
 
-struct order *next_compatible_order(int position, struct order *current, struct order orders[]) {
+Order *next_compatible_order(int position, Order *current, Order orders[]) {
     int i = position;
-    struct order *order_i = &orders[i];
+    Order *order_i = &orders[i];
     while (!does_end_before_start(current, order_i)) {
         ++i;
         order_i = &orders[i];
@@ -79,6 +79,6 @@ struct order *next_compatible_order(int position, struct order *current, struct 
     return order_i;
 }
 
-bool does_end_before_start(struct order *first, struct order *second) {
+bool does_end_before_start(Order *first, Order *second) {
     return first->start_time + first->duration <= second->start_time;
 }
